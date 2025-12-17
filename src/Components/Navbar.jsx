a5ac5fae29a5f7e48c82ce6a713cfd2d3abf4fa4
sobee-dev <obinnasamueledeh10000@gmail.com>
2025-12-17 15:12:46 +0100

    fixed the pop out menu overlay

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,27 +8,25 @@ function Navbar() {
     const [open, setOpen] = useState(false);
     return (
       <>
-      <div className="fixed inset-0 z-60 overflow-hidden">
+      <div className="fixed inset-0 z-60 overflow-hidden pointer pointer-events-none">
         
       
-      <div className='  flex  items-center sticky top-0 z-50 '>
-        <div className={`z-20 absolute  rounded-full bg-blue-800/80 transition-all duration-700 ease-in-out
-            ${open ? "w-[200vw] h-[200vw] -top-[50vw] -right-[50vw] bg-blue-800/80" : "-top-23 -right-21 w-40 h-40"}`}>
+        <div className='  flex  items-center sticky top-0 z-50 pointer-events-auto '>
+          <div className={`z-20 absolute  rounded-full bg-blue-800/80 transition-all duration-700 ease-in-out
+              ${open ? "w-[200vw] h-[200vw] -top-[50vw] -right-[50vw] bg-blue-800/80" : "-top-23 -right-21 w-40 h-40"}`}>
+          </div>
+          <button onClick={() => setOpen(!open)} className="text-white z-30 absolute top-4 right-4">
+              {open ? <FaTimes size={28} /> : <FaBars size={28} />}
+          </button>
+          <ul className={`absolute top-20 right-10 text-white text-lg space-y-4 transition-opacity duration-700 ${
+            open ? "opacity-100 z-40" : "opacity-0 pointer-events-none"}`}>
+          <li><Link to={"/dashboard"}>Home</Link></li> 
+          <li> <Link to={"/cac"}>CAC Registratons</Link></li>
+          <li> <Link to={"/support"}>Contact</Link></li>
+          </ul>  
         </div>
-        <button onClick={() => setOpen(!open)} className="text-white z-30 absolute top-4 right-4">
-            {open ? <FaTimes size={28} /> : <FaBars size={28} />}
-        </button>
-        <ul className={`absolute top-20 right-10 text-white text-lg space-y-4 transition-opacity duration-700 ${
-          open ? "opacity-100 z-40" : "opacity-0 pointer-events-none"}`}>
-         <li><Link to={"/dashboard"}>Home</Link></li> 
-         <li> <Link to={"/cac"}>CAC Registratons</Link></li>
-         <li> <Link to={"/support"}>Contact</Link></li>
-
-        </ul>
-        
       </div>
-      </div>
-    </>
+      </>
     )
     
   }
